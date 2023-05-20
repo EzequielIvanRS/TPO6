b5d695d155c4aa900a5fe3ed8f6d67add0403995
EzequielIvanRS <ezequielivan.rs@gmail.com>
2023-05-20 11:38:07 -0300

    final

--- js/app.js
+++ js/app.js
@@ -1,23 +1,27 @@
 function validar() {
-  valorUno = document.getElementById("nombre").value;
-  valorDos = document.getElementById("mail").value;
-  valorTres = document.getElementById("mensaje").value;
+  let valoruno = document.getElementById("nombre").value;
+  let valordos = document.getElementById("mail").value;
+  let valortres = document.getElementById("mensaje").value;
 
-  if (valorUno == null || valorUno.length == 0) {
-    document.getElementsById("msj").innerHTML = "Por favor complete el nombre";
+  if (valoruno == null || valoruno.length == 0) {
+    document.getElementsById("msjvalidacion1").innerHTML =
+      "Por favor complete el nombre";
     elemento = document.getElementById("nombre");
     elemento.focus();
     return false;
-  } else if (valorDos == null || valorDos.length == 0) {
-    document.getElementsById("msj").innerHTML = "Por favor complete el e-mail";
+  } else if (valordos == null || valordos.length == 0) {
+    document.getElementsById("msjvalidacion2").innerHTML =
+      "Por favor complete el e-mail";
     elemento = document.getElementById("mail");
     elemento.focus();
     return false;
-  } else if (valorTres == null || valorTres.length == 0) {
-    document.getElementsById("msj").innerHTML = "Por favor complete el mensaje";
+  } else if (valortres == null || valortres.length == 0) {
+    document.getElementsById("msjvalidacion3").innerHTML =
+      "Por favor complete el mensaje";
     elemento = document.getElementById("mensaje");
     elemento.focus();
     return false;
+  } else {
+    return true;
   }
-  return true;
 }
