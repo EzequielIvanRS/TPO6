32498bde4b3fbeaf94c51be289090073384554f1
EzequielIvanRS <ezequielivan.rs@gmail.com>
2023-06-19 13:01:49 -0300

    final

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
 function validar() {
-  let valoruno = document.getElementById("nombre").value;
-  let valordos = document.getElementById("mail").value;
-  let valortres = document.getElementById("mensaje").value;
+  var valoruno = document.getElementById("nombre").value;
+  var valordos = document.getElementById("mail").value;
+  var valortres = document.getElementById("mensaje").value;
 
   if (valoruno == null || valoruno.length == 0) {
     document.getElementsById("msjvalidacion1").innerHTML =
       "Por favor complete el nombre";
-    elemento = document.getElementById("nombre");
+    elemento = document.getElementById("nombre").value;
     elemento.focus();
     return false;
   } else if (valordos == null || valordos.length == 0) {
     document.getElementsById("msjvalidacion2").innerHTML =
       "Por favor complete el e-mail";
-    elemento = document.getElementById("mail");
+    elemento = document.getElementById("mail").value;
     elemento.focus();
     return false;
   } else if (valortres == null || valortres.length == 0) {
     document.getElementsById("msjvalidacion3").innerHTML =
       "Por favor complete el mensaje";
-    elemento = document.getElementById("mensaje");
+    elemento = document.getElementById("mensaje").value;
     elemento.focus();
     return false;
   } else {
