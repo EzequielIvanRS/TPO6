c5db63732176efd923bbe35c39f61cf46a3ff294
EzequielIvanRS <128101973+EzequielIvanRS@users.noreply.github.com>
2023-06-25 00:20:18 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4,22 +4,19 @@ function validar() {
   var valortres = document.getElementById("mensaje").value;
 
   if (valoruno == null || valoruno.length == 0) {
-    document.getElementsById("msjvalidacion1").innerHTML =
+    document.getElementById("msjvalidacion1").innerHTML =
       "Por favor complete el nombre";
-    elemento = document.getElementById("nombre").value;
-    elemento.focus();
+    document.getElementById("nombre").focus();
     return false;
   } else if (valordos == null || valordos.length == 0) {
-    document.getElementsById("msjvalidacion2").innerHTML =
+    document.getElementById("msjvalidacion2").innerHTML =
       "Por favor complete el e-mail";
-    elemento = document.getElementById("mail").value;
-    elemento.focus();
+    document.getElementById("mail").focus();
     return false;
   } else if (valortres == null || valortres.length == 0) {
-    document.getElementsById("msjvalidacion3").innerHTML =
+    document.getElementById("msjvalidacion3").innerHTML =
       "Por favor complete el mensaje";
-    elemento = document.getElementById("mensaje").value;
-    elemento.focus();
+    document.getElementById("mensaje").focus();
     return false;
   } else {
     return true;
